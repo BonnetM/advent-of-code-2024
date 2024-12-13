56457578a8704dadd906cc9f71890e94f3f716b0
Maxime Bonnet <maxime.bonnet1@decathlon.com>
2024-12-13 09:23:05 +0100

    add day 13

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,18 @@ import kotlinx.coroutines.runBlocking
 import org.example.day11.BlinkingALot
 import org.example.day12.AreaAndPerimeter
 import org.example.day12.AreaAndSides
+import org.example.day13.LargePositionTokenEquations
+import org.example.day13.TokenEquations
 import kotlin.system.measureTimeMillis
 
 fun main() {
     runBlocking {
-        // 853581 too low
-        val day = "12"
+        val day = "13"
         val inputMode = true
         val filename = "/$day-${if (inputMode) "input" else "example"}.txt"
         println("Solving $filename")
         val time = measureTimeMillis {
-            AreaAndSides().compute(filename)
+            LargePositionTokenEquations().compute(filename)
         }
         println("Completed in $time ms")
     }
---
+++ src/main/kotlin/day13/LargePositionTokenEquations.kt
@@ -0,0 +1,63 @@
+package org.example.day13
+
+class LargePositionTokenEquations {
+
+    private data class Button(val deltaX: Long, val deltaY: Long)
+    private data class Point(val x: Long, val y: Long)
+    private data class System(val buttonA: Button, val buttonB: Button, val prize: Point)
+
+    fun compute(filename: String) {
+        val systems = readFile(filename)
+        val res = systems.sumOf {
+            val r = solve(it)
+            r
+        }
+        println("res = $res")
+
+    }
+
+    private fun solve(system: System): Long {
+        with(system) {
+            val deter = buttonA.deltaX * buttonB.deltaY - buttonA.deltaY * buttonB.deltaX
+            if (deter == 0L) return 0L
+            val numX = prize.x * buttonB.deltaY - prize.y * buttonB.deltaX
+            val numY = prize.y * buttonA.deltaX - prize.x * buttonA.deltaY
+            return if (numX.mod(deter) == 0L && numY.mod(deter) == 0L) {
+                val x = (numX / deter)
+                val y = numY / deter
+
+                3 * x + y
+            } else {
+                0L
+            }
+        }
+    }
+
+    private fun readFile(filename: String): List<System> {
+        return this::class.java.getResourceAsStream(filename)!!.bufferedReader()
+            .readLines()
+            .filter { it.isNotBlank() }
+            .chunked(3) { lines ->
+
+                System(
+                    buttonA = parseButtonLine(lines[0]),
+                    buttonB = parseButtonLine(lines[1]),
+                    prize = parsePrize(lines[2])
+                )
+            }
+    }
+
+    private fun parseButtonLine(line: String): Button {
+        val a = line.split("X+")[1]
+        val x = a.split(",")[0].toLong()
+        val y = a.split("Y+")[1].toLong()
+        return Button(deltaX = x, deltaY = y)
+    }
+
+    private fun parsePrize(line: String): Point {
+        val a = line.split("X=")[1]
+        val x = a.split(",")[0].toLong()
+        val y = a.split("Y=")[1].toLong()
+        return Point(10000000000000L + x, 10000000000000L + y)
+    }
+}
